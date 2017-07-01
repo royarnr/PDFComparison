7412b8d9e0024ce684a05018a6aa7b395040e958
royarnr <arnab.roy4@cognizant.com>
2017-07-01 14:35:12 +0530

    java to json

---
+++ JSONtoPOJO/src/Identity.java
@@ -0,0 +1,16 @@
+
+public class Identity {
+	
+	private Person person;
+	
+	public Person getPerson()
+	{
+		return person;
+	}
+	
+	public void setPerson(Person person)
+	{
+		this.person = person;
+	}
+
+}
